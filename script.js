72e3d0902a63648ff69b95a77e81656904eb92a7
Teja82 <challateja.teju@gmail.com>
2025-05-04 20:17:11 +0530

    Portfolio(Gemini)

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// Basic smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
